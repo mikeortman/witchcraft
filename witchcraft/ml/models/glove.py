99f20813fdf443d424ddde7565a390d15c5bf345
Mike Ortman <mikeortman@gmail.com>
2018-10-26 10:43:27 -0500

    fix syntax error

--- witchcraft/ml/models/glove.py
+++ witchcraft/ml/models/glove.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 
 from witchcraft.nlp.datatypes import Corpus
 from witchcraft.ml.datasets import WitchcraftDatasetIntegerRange
-from witchcraft.ml.optimizers import WitchcraftGradientDescentOptimizer
+from witchcraft.ml.optimizers import WitchcraftAdamOptimizer
 from witchcraft.ml.datatypes import PhraseEmbedding
 from witchcraft.util.protobuf import protobuf_to_filestream
 
@@ -134,7 +134,7 @@ class GloVeModel:
             self._word_embeddings = self._word_embeddings_target + self._word_embeddings_context
             print(self._word_embeddings.shape)
 
-            self._optimizer = WitchcraftGradientDescentOptimizer(0.005).to_tf_optimizer().minimize(self._loss)
+            self._optimizer = WitchcraftAdamOptimizer(0.75).to_tf_optimizer().minimize(self._loss)
             self._summary = tf.summary.scalar("loss", self._loss)
             self._writer = tf.summary.FileWriter('./logs/' + "glove", self._session.graph)
 
