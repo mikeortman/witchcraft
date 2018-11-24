e2341ffe55e8cf0313b5faa3454796db7faa1c43
Mike Ortman <mikeortman@gmail.com>
2018-11-23 22:19:03 -0600

    sometimes you gotta tickle the loss

--- witchcraft/ml/models/fasttext.py
+++ witchcraft/ml/models/fasttext.py
@@ -12,7 +12,7 @@ from witchcraft.util.listutil import skipgramify
 class FastTextHyperparameters:
     def __init__(self) -> None:
         self._embedding_size: int = 300
-        self._batch_size: int = 64
+        self._batch_size: int = 256
         self._window_size: int = 4
         self._subsample_threshold: Optional[float] = None
         self._optimizer: Optimizer = WitchcraftAdamOptimizer(learning_rate=0.001)
@@ -285,36 +285,92 @@ class FastTextModel:
                 target_phrase_ngram_maps = tf.gather(self._phrase_ngram_map, target_phrases)
                 target_phrase_ngram_embeddings = tf.gather(self._ngram_embeddings, target_phrase_ngram_maps)
 
+                print("ES", target_phrase_ngram_embeddings)
+
 
 
                 # Phrase lengths by counting non zero ngram ids
                 with tf.variable_scope("PhraseMaskGeneration"):
                     phrase_length_mask = tf.sign(target_phrase_ngram_maps)
                     phrase_length_mask = tf.maximum(phrase_length_mask, 0)
+                    # phrase_length_mask = tf.Print(phrase_length_mask, [phrase_length_mask], "PM")
 
-                # MASKED ATTENTION
-                with tf.variable_scope("MaskedAttention"):
-                    flattened_phrase = tf.reshape(target_phrase_ngram_embeddings, [self._hyperparameters.get_batch_size(), -1])
+                    phrase_length = tf.reduce_sum(phrase_length_mask, axis=-1)
+                    # phrase_length = tf.Print(phrase_length, [phrase_length], "PL")
+                    # target_phrase_ngram_embeddings /= tf.cast(tf.expand_dims(phrase_length, axis=-1), tf.float32)
+                    # target_phrase_ngram_embeddings = target_phrase_ngram_embeddings * tf.expand_dims(tf.cast(phrase_length_mask, tf.float32), axis=-1)
+                    # attention_optimized_outputs = tf.reduce_sum(target_phrase_ngram_embeddings, axis=-2)
 
-                    with tf.variable_scope("AttentionNeuralNetwork"):
-                        attention = tf.layers.dense(flattened_phrase, 300, activation=tf.tanh)
-                        attention = tf.layers.dense(attention, 128, activation=tf.tanh)
-                        attention = tf.layers.dense(attention, 20, activation=tf.tanh)
-                        attention = tf.squeeze(attention)
 
-                    with tf.variable_scope("AttentionMaskedSoftmax"):
-                        attention = tf.exp(attention)
-                        attention = attention * tf.cast(phrase_length_mask, tf.float32)
-                        attention_denom = tf.reduce_sum(attention, axis=-1, keepdims=True)
-                        attention_denom = tf.maximum(attention_denom, 1e-9)
-                        attention = attention / attention_denom
+                lstm_cell_fw = tf.nn.rnn_cell.LSTMCell(embedding_size/2)
+                lstm_cell_bw = tf.nn.rnn_cell.LSTMCell(embedding_size/2)
+                lstm_initial_state_fw = lstm_cell_fw.zero_state(self._hyperparameters.get_batch_size(), dtype=tf.float32)
+                lstm_initial_state_bw = lstm_cell_bw.zero_state(self._hyperparameters.get_batch_size(), dtype=tf.float32)
 
-                    attention = tf.Print(attention, [attention], "AT")
+                _, (final_state_fw, final_state_bw) = tf.nn.bidirectional_dynamic_rnn(
+                                                                                                lstm_cell_fw,
+                                                                                                lstm_cell_bw,
+                                                                                                target_phrase_ngram_embeddings,
+                                                                                                sequence_length=phrase_length,
+                                                                                                initial_state_fw=lstm_initial_state_fw,
+                                                                                                initial_state_bw=lstm_initial_state_bw)
 
-                    with tf.variable_scope("AttentionOptimizedEmbeddingGen"):
-                        attention_optimized_outputs = target_phrase_ngram_embeddings * tf.expand_dims(attention, axis=-1)
-                        attention_optimized_outputs = tf.reduce_sum(attention_optimized_outputs, axis=-2)
+                lstm_final_outputs = tf.concat([final_state_fw.h, final_state_bw.h], axis=-1)
 
+                print("LSTM_FINAL_OUTPUTS", lstm_final_outputs)
+                lstm_final_outputs = tf.Print(lstm_final_outputs, [lstm_final_outputs], "LSTM")
+
+                lstm_overextension = tf.reduce_sum(tf.square(tf.maximum(tf.abs(lstm_final_outputs), 0.8) - 0.8))
+                lstm_overextension = tf.Print(lstm_overextension, [lstm_overextension], "LSTM_OVEREXTENSION")
+
+                lstm_final_outputs_norm = tf.nn.l2_normalize(lstm_final_outputs, axis=-1)
+                # lstm_final_outputs_norm = tf.Print(lstm_final_outputs, [lstm_final_outputs_norm], "LSTM_NORM")
+
+
+
+                # Hacky stuff. Add some loss to prevent individual features from converging to the same value.
+                lstm_mean, lstm_variance = tf.nn.moments(lstm_final_outputs_norm, [-2])
+                # lstm_variance = tf.Print(lstm_variance, [lstm_variance], "LSTM_VARIANCE")
+                lstm_stddev = tf.sqrt(lstm_variance)
+                # lstm_stddev = tf.Print(lstm_stddev, [lstm_stddev], "LSTM_STDDEV")
+                lstm_stddev_capped = tf.minimum(lstm_stddev, 0.5)
+                # lstm_stddev_capped = tf.Print(lstm_stddev_capped, [lstm_stddev_capped], "LSTM_STDDEV_CAPPED")
+
+                lstm_stddev_loss = tf.reduce_sum(lstm_stddev_capped, axis=-1)
+                lstm_stddev_loss = tf.Print(lstm_stddev_loss, [lstm_stddev_loss], "LSTM_STDLOSS")
+
+
+                # # MASKED ATTENTION
+                # with tf.variable_scope("MaskedAttention"):
+                #     target_phrase_ngram_embeddings = target_phrase_ngram_embeddings * tf.expand_dims(tf.cast(phrase_length_mask, tf.float32), axis=-1)
+                #     flattened_phrase = tf.reshape(target_phrase_ngram_embeddings, [self._hyperparameters.get_batch_size(), -1])
+                #
+                #     with tf.variable_scope("AttentionNeuralNetwork"):
+                #         attention = tf.layers.dense(flattened_phrase, 300, activation=tf.nn.relu)
+                #         attention = tf.layers.dense(attention, 128, activation=tf.nn.relu)
+                #         attention = tf.layers.dense(attention, 20, activation=tf.nn.relu)
+                #         attention = tf.squeeze(attention)
+                #
+                #     with tf.variable_scope("AttentionMaskedSoftmax"):
+                #         attention = tf.exp(attention)
+                #         attention = attention * tf.cast(phrase_length_mask, tf.float32)
+                #         attention_denom = tf.reduce_sum(attention, axis=-1, keepdims=True)
+                #         attention_denom = tf.maximum(attention_denom, 1e-9)
+                #         attention = attention / attention_denom
+                #
+                #     attention = tf.Print(attention, [attention], "AT")
+                #
+                #     with tf.variable_scope("AttentionOptimizedEmbeddingGen"):
+                #         attention_optimized_outputs = target_phrase_ngram_embeddings * tf.expand_dims(attention, axis=-1)
+                #         attention_optimized_outputs = tf.reduce_sum(attention_optimized_outputs, axis=-2)
+
+
+                # with tf.variable_scope("Contextualization"):
+                #     context_aware_output = tf.layers.dense(attention_optimized_outputs, 300, activation=tf.nn.relu)
+                #     context_aware_output = tf.layers.dense(context_aware_output, 300,
+                #                                            activation=tf.nn.relu,
+                #                                            kernel_regularizer=tf.contrib.layers.l2_regularizer(scale=0.1),
+                #                                            activity_regularizer=tf.contrib.layers.l2_regularizer(scale=0.1))
 
                 phrase_nce_weights = tf.Variable(
                     tf.truncated_normal(
@@ -326,24 +382,29 @@ class FastTextModel:
 
                 phrase_nce_bias = tf.Variable(tf.zeros([self._vocab.get_vocab_size()]), name="NoiseConstrastiveBiases")
 
+
                 self._nce_phrase_loss_batch = tf.nn.nce_loss(
                     weights=phrase_nce_weights,
                     biases=phrase_nce_bias,
                     labels=positive_phrases_context_labels,
-                    inputs=attention_optimized_outputs,
+                    inputs=lstm_final_outputs,
                     num_sampled=64,
                     num_classes=self._vocab.get_vocab_size(),
                     name="NoiseContrastiveLoss"
                 )
 
-                self._nce_phrase_loss = tf.reduce_sum(tf.square(self._nce_phrase_loss_batch))
+                self._nce_phrase_loss = tf.reduce_mean(self._nce_phrase_loss_batch) / lstm_stddev_loss + 100 *lstm_overextension
                 self._summary_phrase_loss = tf.summary.scalar("phrase_loss_mean", tf.reduce_sum(self._nce_phrase_loss_batch) / self._hyperparameters.get_batch_size())
+                self._summary_phrase_lstm_stddev = tf.summary.scalar("lstm_stddev_loss", lstm_stddev_loss)
+                self._summary_phrase_overextension = tf.summary.scalar("lstm_overextension", lstm_overextension)
+                self._summary_phrase = tf.summary.merge([self._summary_phrase_loss, self._summary_phrase_lstm_stddev, self._summary_phrase_overextension])
+
                 self._optimizer_phrase_loss = hyperparameters.get_optimizer().to_tf_optimizer()
                 self._minimize_phrase_loss = self._optimizer_phrase_loss.minimize(self._nce_phrase_loss, var_list=tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope=scope_phrase.name))
 
-                # trainable_vars =  tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope=vs.name)
+                # trainable_vars =  tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope=scope_phrase.name)
                 # grads = tf.gradients(self._nce_phrase_loss, trainable_vars)
-                # grads, _ = tf.clip_by_global_norm(grads, 0.001)  # gradient clipping
+                # grads, _ = tf.clip_by_global_norm(grads, 0.01)  # gradient clipping
                 # grads_and_vars = list(zip(grads, trainable_vars))
                 # self._minimize_phrase_loss = self._optimizer_phrase_loss.apply_gradients(grads_and_vars)
 
@@ -365,11 +426,11 @@ class FastTextModel:
     def train(self, global_step: int) -> None:
         with self._graph.as_default():
 
-            if global_step <= 1000000:
+            if global_step <= 2000:
                 _, calc_summary = self._session.run([self._minimize_ngram_loss, self._summary_ngram_loss])
                 self._writer.add_summary(calc_summary, global_step=global_step)
             else:
-                _, calc_summary = self._session.run([self._minimize_phrase_loss, self._summary_phrase_loss])
+                _, calc_summary = self._session.run([self._minimize_phrase_loss, self._summary_phrase])
                 self._writer.add_summary(calc_summary, global_step=global_step)
 
             if global_step % 1000 == 0:
